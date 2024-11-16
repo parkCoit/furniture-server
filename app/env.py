a08b84690b88529a9e97bd42dc6a145258e51180
coitpark <minhobag18@gmail.com>
2024-11-16 15:08:09 +0900

    Remove tracked files and update .gitignore

--- app/env.py
+++
@@ -1,9 +0,0 @@
-# "localhost"
-USERNAME = "coit"
-PASSWORD = "coit"
-HOSTNAME = "db"
-DATABASE = "furniture_db"
-PORT = 3306
-CHARSET = "utf8"
-DB_URL = f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}"
-
